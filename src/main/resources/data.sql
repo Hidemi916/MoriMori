eedddb91af53e4d9e36edbf73f1a8b459717f360
d-hidemi <d_kammosyu@gmail.com>
2024-05-30 10:18:09 +0900

    1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO sample (date, naiyou, yen) VALUES ('2024-05-27', 'naiyou', 0);
+INSERT INTO sample (date, naiyou, yen) VALUES ('2024-05-6', '食事代', 1250);
+INSERT INTO sample (date, naiyou, yen) VALUES ('2024-05-6', '美容院代', 13000);
+INSERT INTO sample (date, naiyou, yen) VALUES ('2024-05-15', '洋服代', 6200);
+INSERT INTO sample (date, naiyou, yen) VALUES ('2024-05-21', 'ネイル代', 8700);
+INSERT INTO sample (date, naiyou, yen) VALUES ('2024-05-27', 'チケット代', 13500);
